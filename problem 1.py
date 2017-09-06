90de97d93e0df24e6a9ba250c6369dd35f35e792
yehudav <30637657+yehudav@users.noreply.github.com>
2017-09-06 17:43:20 +0300

    Update problem 1.py

--- problem 1.py
+++ problem 1.py
@@ -8,3 +8,6 @@ multiples_of_five = 5 * arithmetic_sequence_sum(199)
 multiples_of_fifteen = 15 * arithmetic_sequence_sum(66)
 
 print(multiples_of_three + multiples_of_five - multiples_of_fifteen )
+
+
+
