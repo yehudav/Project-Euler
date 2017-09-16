b1121fafd4d43484c61e0d22073e2951572891cb
yehudav <30637657+yehudav@users.noreply.github.com>
2017-09-17 00:15:14 +0300

    Add files via upload

---
+++ problem 28.py
@@ -0,0 +1,25 @@
+
+
+nums = []
+diagnols_sum = 0
+jump = 2
+
+for i in range(1, 1002002):
+    nums.append(i)
+
+j = 0
+jump_index = 0
+
+while j < 1002001:
+    if jump_index == 4:
+        jump += 2
+        jump_index = 0
+
+    diagnols_sum += nums[j]
+    jump_index += 1
+    j += jump
+
+print(diagnols_sum)
+
+
+
