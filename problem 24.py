a5c4b19a7b4c2d380a215d1e584006b1d13c7a2b
yehudav <yehuda.ar.v@gmail.com>
2018-01-31 15:51:59 +0200

    update

--- problem 24.py
+++ problem 24.py
@@ -16,19 +16,25 @@ def find_i_permutation(elements, i):
 print(find_i_permutation([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 1000000))
 
 
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+
 def find_i_permutation_v2(elements, i):
     i_permutation = ""
     n = len(elements)
     cur_sum = 0
 
     for k in range(1, n):
-        factorial = f(n - k)
+        cur_factorial = factorial(n - k)
 
         for e in elements:
-            cur_sum += factorial
+            cur_sum += cur_factorial
 
             if cur_sum >= i:
-                cur_sum -= factorial
+                cur_sum -= cur_factorial
                 i_permutation += str(e)
                 elements.remove(e)
                 break
