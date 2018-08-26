889e4ac802424a7ee71ebe3fd5f77466de981f7c
yehudav <yehuda.ar.v@gmail.com>
2018-08-26 19:42:20 +0300

    update

---
+++ problem 102.py
@@ -0,0 +1,27 @@
+"""         problem 102           """
+
+
+def oa(x1, y1, x2, y2):
+    k = 0.5 *
+
+    if k < 0:
+        return -1
+    if k > 0:
+        return 1
+    return 0
+
+
+def mi(path):
+    file = open(path, "r")
+    lis = []
+    sum = 0
+    for line in file:
+        x1, y1, x2, y2, x3, y3 = line
+        val = oa(int(x1), int(y1), int(x2), int(y2)) + oa(int(x2), int(y2), int(x3), int(y3)) + oa(int(x3), int(y3),
+                                                                                                   int(x1), int(y1))
+        if abs(val) == 3:
+            sum += 1
+    return sum
+
+
+print(mi)
---
+++ problem 99.py
@@ -0,0 +1,30 @@
+"""         problem 99           """
+
+
+def load_bases_and_exponents(path):
+    file = open(path, "r")
+    bases_and_exps_pairs = []
+    line_num = 0
+    for pair in file:
+        base, exp = pair
+        bases_and_exps_pairs.append((int(base), int(exp), line_num))
+        line_num += 1
+    return bases_and_exps_pairs
+
+
+def get_max_base(pair1, pair2):
+    if pair1[0] < pair2[0] and pair1[1] < pair2[1]:
+        return pair2
+    if pair1[0] > pair2[0] and pair1[1] > pair2[1]:
+            return pair1
+
+    
+
+    return pair1
+
+def get_max_numeric_line_num(path):
+    bases = load_bases_and_exponents(path)
+    max_base = bases[0]
+    for pair in bases:
+        max_base = get_max_base(max_base, pair)
+    return max_base[2]
