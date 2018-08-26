ed532bee591383295f01fa7fdc4bdae35d69219d
yehudav <yehuda.ar.v@gmail.com>
2018-08-26 21:11:06 +0300

    update

--- problem 2.py
+++ problem 2.py
@@ -1,22 +1,19 @@
 """         problem 2           """
 
 
-def sum_of_even_fibonacci_numbers(bound):
-    sum_of_even_numbers = 0
-    fn_minus_2 = 0
-    fn_minus_1 = 1
+def is_even(n):
+    return n / 2 == n // 2
 
+def sum_of_even_fibonacci_numbers(bound, fn_minus_2, fn_minus_1):
+    sum_of_even_numbers = 0
     while fn_minus_2 < bound:
         fn = fn_minus_1 + fn_minus_2
         fn_minus_2 = fn_minus_1
         fn_minus_1 = fn
-
-        if fn_minus_2 % 2 == 0:
+        if is_even(fn_minus_2):
             sum_of_even_numbers += fn_minus_2
-
     return sum_of_even_numbers
 
 
-print(sum_of_even_fibonacci_numbers(4000000))
-
+print(sum_of_even_fibonacci_numbers(4000000, 0, 1))
 
