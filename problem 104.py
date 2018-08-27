30d1544a1f6c83f77779d326485d10eb7a4a9cce
yehudav <yehuda.ar.v@gmail.com>
2018-08-27 13:57:15 +0300

    update

---
+++ problem 104.py
@@ -0,0 +1,11 @@
+"""         problem 104           """
+import math
+
+
+def fib():
+    sq = math.sqrt(5)
+    n = 1474
+    f2 = (1 / sq) * (((sq + 1) / 2) ** n - ((sq - 1) / 2) ** n)
+    return f2
+
+print(fib())
