6bfd1af76f8fdc5db31d8e07e8cb67ba919d13b2
yehudav <yehuda.ar.v@gmail.com>
2018-08-28 23:01:22 +0300

    update

--- problem 27.py
+++ problem 27.py
@@ -2,3 +2,35 @@
 
 # b most be prime
 # n + a < b   or  n - a < b
+
+
+import math
+
+
+def sieve_of_eranthoses(bound):
+    primes = [0, 0, 0]
+    i = 3
+    while i < bound:
+        primes.append(i)
+        primes.append(0)
+        i += 2
+
+    for j in range(3, int(math.sqrt(bound))):
+        k = j + j
+        while k < bound:
+            primes[k] = 0
+            k += j
+    return primes
+
+
+def remove_zeros(lis):
+    new_lis = []
+    for i in lis:
+        if i != 0:
+            new_lis.append(i)
+    return new_lis
+
+
+def primes(bound):
+    primes = remove_zeros(sieve_of_eranthoses(bound))
+    for b in primes:
