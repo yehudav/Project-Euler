7a31fe1f0ce152ddb442bcb1d2ece77aa798d47b
yehudav <yehuda.ar.v@gmail.com>
2018-08-31 15:56:51 +0300

    update

---
+++ problem 73.py
@@ -0,0 +1,14 @@
+import math
+
+
+def x():
+    lis = set()
+    for d in range(2, 12001):
+        for n in range(d // 3, d // 2 + 1):
+            if math.gcd(n, d) == 1:
+                lis.add(n / d)
+    lis = sorted(list(lis))
+    print(len(lis[lis.index(1 / 3):lis.index(1 / 2) - 1]))
+
+
+x()
