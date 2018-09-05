7b1a00f33c8a2ce4b9f2a680d03e27a8f6a09d87
yehudav <yehuda.ar.v@gmail.com>
2018-09-06 00:54:42 +0300

    update

--- problem 71.py
+++ problem 71.py
@@ -4,11 +4,26 @@ import math
 def x(bound):
     lis = set()
     for d in range(2, bound + 1):
-        for n in range(int(d / 2.5), d // 2):
+        for n in range(math.floor(d * 0.4), math.ceil(d * 0.43) + 1):
             if math.gcd(n, d) == 1:
                 lis.add(n / d)
     lis = sorted(list(lis))
-    print(lis.index(3 / 7) - 1)
+    print(lis.index(3 / 7) - 1) 
 
 
 x(10 ** 7)
+#
+# import math
+#
+#
+# def x():
+#     lis = set()
+#     for d in range(2, 12001):
+#         for n in range(d // 3, d // 2 + 1):
+#             if math.gcd(n, d) == 1:
+#                 lis.add(n / d)
+#     lis = sorted(list(lis))
+#     print(len(lis[lis.index(1 / 3):lis.index(1 / 2) - 1]))
+#
+#
+# x()
