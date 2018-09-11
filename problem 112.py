b9d43262e976f20996e5e9c13f63bc8f15694eb6
yehudav <yehuda.ar.v@gmail.com>
2018-09-12 01:09:41 +0300

    update

---
+++ problem 112.py
@@ -0,0 +1,41 @@
+"""         problem 112           """
+
+
+def get_bouncy_percent_num(percent):
+    cur_percent = 0
+    bouncy_num = 0
+    i = 99
+    while cur_percent != percent:
+        i += 1
+        if is_bouncy(i):
+            bouncy_num += 1
+        cur_percent = get_cur_percent(i, bouncy_num)
+    return i
+
+
+def is_bouncy(n):
+    return not is_dec(n) and not is_inc(n)
+
+
+def is_dec(n):
+    st = str(n)
+    for j in range(len(st) - 1):
+        if int(st[j]) < int(st[j + 1]):
+            return False
+    return True
+
+
+def is_inc(n):
+    st = str(n)
+    for j in range(len(st) - 1):
+        if int(st[j]) > int(st[j + 1]):
+            return False
+    return True
+
+
+def get_cur_percent(number, bouncy_number):
+    ratio = bouncy_number / (number)
+    return ratio
+
+
+print(get_bouncy_percent_num(0.99))
