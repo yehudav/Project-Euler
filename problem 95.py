cd74802507960be9ad0f02b6d29afcf0203bcd52
yehudav <yehuda.ar.v@gmail.com>
2018-09-21 01:41:46 +0300

    update

---
+++ problem 95.py
@@ -0,0 +1,35 @@
+def longest_chain_min(bound):
+    cur_min = 0
+    max_length = 0
+    for i in range(4, bound):
+        chain = get_chain(i)
+        if len(chain) > max_length:
+            max_length = len(chain)
+            cur_min = min(chain)
+    return cur_min
+
+
+def get_chain(n):
+    i = 0
+    s = set()
+    cur = n
+    while len(s) == i:
+        s.add(cur)
+        cur = div_sum(cur)
+        i += 1
+    return s
+
+
+def div_sum(n):
+    sum = 0
+    for i in range(1, int(n / 2) + 1):
+        if evenly_divisible(n, i):
+            sum += i
+        return sum
+
+
+def evenly_divisible(n, i):
+    return n / i == n // i
+
+
+print(longest_chain_min(1000000))
