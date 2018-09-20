5394c456993848acf92326b1a36ed5e1bdf518e6
yehudav <yehuda.ar.v@gmail.com>
2018-09-20 23:34:40 +0300

    update

---
+++ problem 57.py
@@ -0,0 +1,21 @@
+from fractions import *
+
+
+def square_root_longer_numerator(n):
+    num = 0
+    cur = Fraction(3, 2)
+    for i in range(n):
+        cur += 1
+        cur = 1 / cur + 1
+        if is_numerator_longer(cur.denominator, cur.numerator):
+            num += 1
+    return num
+
+
+def is_numerator_longer(dd, nn):
+    if len(str(nn)) > len(str(dd)):
+        return True
+    return False
+
+
+print(square_root_longer_numerator(1000))
--- problem 7.py
+++ problem 7.py
@@ -1,21 +1,18 @@
-def is_prime(num, primes_list):
-    for prime in primes_list:
-        if num % prime == 0:
-            return False
-    return True
-
-
 def find_prime(index):
     primes = [2]
     cur = 3
-
     while len(primes) < index:
         if is_prime(cur, primes):
             primes.append(cur)
-
         cur += 2
-
     return primes[-1]
+
+
+def is_prime(num, primes):
+    for prime in primes:
+        if num % prime == 0:
+            return False
+    return True
 
 
 print(find_prime(10001))
