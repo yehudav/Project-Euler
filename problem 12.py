7e28ce21daed1959d0dfcd88f073b4763fd0a129
yehudav <yehuda.ar.v@gmail.com>
2018-11-25 11:25:32 +0200

    refactored

--- problem 12.py
+++ problem 12.py
@@ -1,11 +1,19 @@
 import math
 
 
-def is_prime(num, primes):
-    for p in primes:
-        if num % p == 0:
-            return False
-    return True
+def first_triangle_num_with_n_divisors(n, primes_bound):
+    primes = get_primes(primes_bound)
+    first_triangle = 0
+    i = 1
+
+    while first_triangle == 0:
+        current_triangle = triangle_num(i)
+        if get_num_of_divisors(current_triangle, primes) > n:
+            first_triangle = current_triangle
+
+        i += 1
+
+    return first_triangle
 
 
 def get_primes(primes_bound):
@@ -22,11 +30,18 @@ def get_primes(primes_bound):
     return primes
 
 
+def is_prime(num, primes):
+    for p in primes:
+        if num % p == 0:
+            return False
+    return True
+
+
 def triangle_num(n):
     return int((n * (n + 1)) / 2)
 
 
-def divisors_number(n, primes):
+def get_num_of_divisors(n, primes):
     divs = 1
     cur = n
 
@@ -49,19 +64,4 @@ def divisors_number(n, primes):
     return divs
 
 
-def first_triangle_num_with_n_divisors(n, primes_bound):
-    primes = get_primes(primes_bound)
-    first_triangle = 0
-    i = 1
-
-    while first_triangle == 0:
-        current_triangle = triangle_num(i)
-        if divisors_number(current_triangle, primes) > n:
-            first_triangle = current_triangle
-
-        i += 1
-
-    return first_triangle
-
-
-print(first_triangle_num_with_n_divisors(500, 100000))
+print(first_triangle_num_with_n_divisors(500, 500))
--- problem 20.py
+++ problem 20.py
@@ -1,17 +1,19 @@
+def factorial_digits_sum(num):
+    factorial_result = str(factorial(num))
+    return sum_of_digits(factorial_result)
+
+
 def factorial(n):
     if n == 1:
         return 1
     return n * factorial(n - 1)
 
 
-def factorial_digits_sum(num):
-    factorial_num = str(factorial(num))
-    sum_of_digits = 0
-
-    for digit in factorial_num:
-        sum_of_digits += int(digit)
-
-    return sum_of_digits
+def sum_of_digits(n):
+    digits_sum = 0
+    for digit in n:
+        digits_sum += int(digit)
+    return digits_sum
 
 
 print(factorial_digits_sum(100))
