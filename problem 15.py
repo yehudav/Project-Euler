bd060fdcfa075686ce7e77708d1b1e25d307c28f
yehudav <yehuda.ar.v@gmail.com>
2018-12-05 12:27:01 +0200

    refactored

--- problem 15.py
+++ problem 15.py
@@ -1,11 +1,11 @@
+def choose(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - k))
+
+
 def factorial(n):
     if n == 1:
         return 1
     return n * factorial(n - 1)
-
-
-def choose(n, k):
-    return int(factorial(n) / (factorial(k) * factorial(n - k)))
 
 
 print(choose(40, 20))
--- problem 21.py
+++ problem 21.py
@@ -1,25 +1,28 @@
+def amicable_numbers_sum(bound):
+    amicable_sum = 0
+    for cur in range(1, bound):
+        if is_amicable(cur):
+            amicable_sum += cur
+    return amicable_sum
+
+
+def is_amicable(n):
+    div_sum = sum_of_divisors(n)
+    n_tag = sum_of_divisors(div_sum)
+    return n_tag == n and n_tag != div_sum
+
+
 def sum_of_divisors(num):
-    bound = num // 2 + 1
+    upper_bound = num // 2 + 1
     divisors_sum = 0
-
-    for i in range(1, bound):
-        if num % i == 0:
+    for i in range(1, upper_bound):
+        if evenly_divisible(num, i):
             divisors_sum += i
-
     return divisors_sum
 
 
-def amicable_numbers_sum(bound):
-    amicable_sum = 0
-
-    for cur in range(1, bound):
-        cur_div_sum = sum_of_divisors(cur)
-        cur_tag = sum_of_divisors(cur_div_sum)
-
-        if cur_tag == cur and cur_tag != cur_div_sum:
-            amicable_sum += cur
-
-    return amicable_sum
+def evenly_divisible(n, d):
+    return n / d == n // d
 
 
 print(amicable_numbers_sum(10000))
