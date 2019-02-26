c570f0dc5238dfe2be5e9a273a8fa062bbf59f1b
yehudav <yehuda.ar.v@gmail.com>
2019-02-26 14:31:14 +0200

    refactored

--- problem 25.py
+++ problem 25.py
@@ -1,14 +1,24 @@
-def n_digits_fibonacci(n):
-    fn_minus_2 = 1
-    fn_minus_1 = 1
-    counter = 2
+class Fibonaci_first_n_digits_term_finder:
+    def __init__(self):
+        self.fn_minus_2 = 1
+        self.fn_minus_1 = 1
+        self.index = 2
+        self.length = None
 
-    while len(str(fn_minus_1)) < n:
-        fn_minus_1 += fn_minus_2
-        fn_minus_2 = fn_minus_1 - fn_minus_2
-        counter += 1
+    def get_first_n_digits_fibonacci(self, n=None):
+        self.length = n
+        while self.not_n_digits_long():
+            self.update_cur_term()
+        return self.index
 
-    return counter
+    def not_n_digits_long(self):
+        return len(str(self.fn_minus_1)) < self.length
+
+    def update_cur_term(self):
+        self.fn_minus_1 += self.fn_minus_2
+        self.fn_minus_2 = self.fn_minus_1 - self.fn_minus_2
+        self.index += 1
 
 
-print(n_digits_fibonacci(1000))
+finder = Fibonaci_first_n_digits_term_finder()
+print(finder.get_first_n_digits_fibonacci(1000))
--- problem 29.py
+++ problem 29.py
@@ -1,10 +1,19 @@
-distinct_numbers = []
+def get_distinct_powers_in_range(lower_bound, upper_bound):
+    upper_bound = upper_bound + 1
+    all_powers = get_all_powers(lower_bound, upper_bound)
+    return get_distinct_powers_num(all_powers)
 
-for i in range(99):
-    a = i + 2
-    for j in range(99):
-        b = j + 2
-        if a ** b not in distinct_numbers:
-            distinct_numbers.append(a ** b)
 
-print(len(distinct_numbers))
+def get_all_powers(lower_bound, upper_bound):
+    all_powers = []
+    for a in range(lower_bound, upper_bound):
+        for b in range(lower_bound, upper_bound):
+            all_powers.append(a ** b)
+    return all_powers
+
+
+def get_distinct_powers_num(numbers):
+    return len(set(numbers))
+
+
+print(get_distinct_powers_in_range(2, 100))
