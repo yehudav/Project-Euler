092dc12c264c45cd25e100e0a6264e22c1779faf
yehud <yehuda.ar.v@gmail.com>
2020-06-24 17:33:33 +0300

    refactored

--- 0014.py
+++ 0014.py
@@ -1,34 +1,53 @@
-def get_longest_chain_head_under_bound(bound):
-    lengths_by_indexes = get_chains_lengths(bound)
-    return get_longest_chain_head(lengths_by_indexes)
+import util
 
 
-def get_chains_lengths(bound):
-    upper_bound = bound + 1
-    chains_lengths = []
-    for i in range(upper_bound):
-        chains_lengths.append(chain_length(i))
-    return chains_lengths
+class LongestCollatzSequence:
+    def __init__(self):
+        self.dic = None
+        self.bound = None
 
+    def get_longest_chain_head_under_bound(self, bound):
+        if bound < 2:
+            raise ValueError
+        self.bound = bound
+        self.dic = {i: 0 for i in range(1, self.bound)}
+        self.calculate_chains()
+        return self.get_longest_chain_head()
 
-def chain_length(head):
-    chain_len = 0
-    cur_num = head
-    while cur_num > 1:
-        cur_num = get_next_num_in_chain(cur_num)
-        chain_len += 1
-    return chain_len
+    def calculate_chains(self):
+        for i in range(2, self.bound):
+            self.calculate_chain_length(i)
 
+    def calculate_chain_length(self, head):#todo refactor
+        chain = []
+        cur = head
+        while cur != 1:
+            if self.dic[cur] > 0:
+                break
+            chain.append(cur)
+            cur = self.get_next_num_in_chain(cur)
+            if cur not in self.dic:
+                self.dic[cur] = 0
+        for i in chain[::-1]:
+            self.dic[i] = self.dic[cur] + 1
+            cur = i
+        return self.dic[head]
 
-def get_next_num_in_chain(n):
-    if n != ((n >> 1) << 1):
-        return n + n + n + 1
-    else:
+    def get_next_num_in_chain(self, n):
+        if util.is_odd_num(n):
+            return 3 * n + 1
         return n >> 1
 
+    def get_longest_chain_head(self):
+        max_len_head = 1
+        for key in self.dic:
+            if self.dic[key] > self.dic[max_len_head]:
+                max_len_head = key
+        return max_len_head
 
-def get_longest_chain_head(chains_lengths):
-    return chains_lengths.index(max(chains_lengths))
 
-
-print(get_longest_chain_head_under_bound(1000000))
+if __name__ == "__main__":
+    n = 1000000
+    longest_collatz_seq = LongestCollatzSequence()
+    print(longest_collatz_seq.get_longest_chain_head_under_bound(n))
+    print(longest_collatz_seq.get_longest_chain_head_under_bound(n) == 837799)
